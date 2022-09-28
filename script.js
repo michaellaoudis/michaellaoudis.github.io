90cacfd2b16cf371f182fd93b4358baa0c31e0dd
Michael Laoudis <michaellaoudis@gmail.com>
2022-09-28 16:29:03 -0700

    Updated: certifications section, top expertise items, hackerone contact. Added dropdown arrow to each cyber-security competency section.

--- script.js
+++ script.js
@@ -1,6 +1,107 @@
-var themeDots = document.getElementsByClassName('theme-dot')
+var themeDots = document.getElementsByClassName('theme-dot');
+var theme = localStorage.getItem('theme');
 
-var theme = localStorage.getItem('theme')
+isShow = false;
+// Adv TCP/IP
+function showHideTcpPage() {
+    manSection = document.querySelector(".toggle-TCP-content");
+    var arrow = document.getElementById('tcp-arrow')
+    if(isShow){
+        arrow.className = 'fa fa-arrow-down';
+        manSection.style.display = "none";
+        isShow = false;
+    } else{
+        arrow.className = 'fa fa-arrow-down open';
+        manSection.style.display = "block";
+        isShow = true;
+    }
+}
+// Contingency Planning
+function showHideContinPage() {
+    manSection = document.querySelector(".toggle-Contin-content");
+    var arrow = document.getElementById('contin-arrow')
+    if(isShow){
+        arrow.className = 'fa fa-arrow-down';
+        manSection.style.display = "none";
+        isShow = false;
+    } else{
+        arrow.className = 'fa fa-arrow-down open';
+        manSection.style.display = "block";
+        isShow = true;
+    }
+}
+
+// Attack Techniques
+function showHideAttackPage() {
+    manSection = document.querySelector(".toggle-Attack-content");
+    var arrow = document.getElementById('atk-arrow')
+    if(isShow){
+        arrow.className = 'fa fa-arrow-down';
+        manSection.style.display = "none";
+        isShow = false;
+    } else{
+        arrow.className = 'fa fa-arrow-down open';
+        manSection.style.display = "block";
+        isShow = true;
+    }
+}
+// Sec Cisco Routers
+function showHideCiscoPage() {
+    manSection = document.querySelector(".toggle-Cisco-content");
+    var arrow = document.getElementById('cisco-arrow')
+    if(isShow){
+        arrow.className = 'fa fa-arrow-down';
+        manSection.style.display = "none";
+        isShow = false;
+    } else{
+        arrow.className = 'fa fa-arrow-down open';
+        manSection.style.display = "block";
+        isShow = true;
+    }
+}
+// Harden Windows
+function showHideWinPage() {
+    manSection = document.querySelector(".toggle-Win-content");
+    var arrow = document.getElementById('win-arrow')
+    if(isShow){
+        arrow.className = 'fa fa-arrow-down';
+        manSection.style.display = "none";
+        isShow = false;
+    } else{
+        arrow.className = 'fa fa-arrow-down open';
+        manSection.style.display = "block";
+        isShow = true;
+    }
+}
+// Harden Linux
+function showHideLinPage() {
+    manSection = document.querySelector(".toggle-Lin-content");
+    var arrow = document.getElementById('lin-arrow')
+    if(isShow){
+        arrow.className = 'fa fa-arrow-down';
+        manSection.style.display = "none";
+        isShow = false;
+    } else{
+        arrow.className = 'fa fa-arrow-down open';
+        manSection.style.display = "block";
+        isShow = true;
+    }
+}
+// Internet Security WWW
+function showHideWWWPage() {
+    manSection = document.querySelector(".toggle-WWW-content");
+    var arrow = document.getElementById('www-arrow')
+    if(isShow){
+        arrow.className = 'fa fa-arrow-down';
+        manSection.style.display = "none";
+        isShow = false;
+    } else{
+        arrow.className = 'fa fa-arrow-down open';
+        manSection.style.display = "block";
+        isShow = true;
+    }
+}
+
 
 if(theme == null){
     setTheme('light')
